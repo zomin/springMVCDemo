646cc8fcaa7badb7b4ce6a9add498034facac20d
Zhang <zhan005@asycn.loc>
2016-06-02 16:21:15 +0800

    add slf4j 修改 日志框架 slf4j+log4j2 druid升级1.0.20 将数据源修改成druid

--- src/main/java/com/example/controller/ExampleController.java
+++ src/main/java/com/example/controller/ExampleController.java
@@ -2,15 +2,14 @@ package com.example.controller;
 
 import com.example.model.User;
 import com.example.service.ExampleService;
-import com.example.service.ProService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,21 +19,27 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class ExampleController {
     private static final String TAG = "ExampleController";
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleController.class);
     @Autowired
     private ExampleService exampleService;
 
-    @Autowired
-    private ProService proService;
+//    @Autowired
+//    private ProService proService;
 
     @RequestMapping("/login")
     public ModelAndView getUser(HttpServletRequest request, HttpServletResponse response){
+        if(LOGGER.isInfoEnabled()){
+            LOGGER.info(TAG+" Start!");
+        }
         String userId = request.getParameter("userId");
         User user = exampleService.get(userId);
-        String masg = proService.sayHello("123123");
-        user.setUserName(masg);
+//        String masg = proService.sayHello("123123");
+//        user.setUserName(masg);
         ModelAndView mv = new ModelAndView("login");
         mv.addObject("user",user);
+        if(LOGGER.isInfoEnabled()){
+            LOGGER.info(TAG+" End!");
+        }
         return mv;
     }
 } 
